b8e33fcc7bdba5a5cc24a16a47c1f6eabb21a2cb
Jenni <jennyprzg2000.jh@gmail.com>
2021-08-09 20:54:08 -0600

    Inicia proyecto

---
+++ src/poofiguras/Principal.java
@@ -0,0 +1,11 @@
+package poofiguras;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Figuras datos = new Figuras();
+        datos.cuadrado();
+        datos.triangulo();
+    }
+
+}
